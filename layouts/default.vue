d9a2af5f4a37a061584fd9f6e68bee92c9260468
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2021-03-31 16:21:48 +0900

    Fix

--- layouts/default.vue
+++ layouts/default.vue
@@ -18,20 +18,22 @@
       <v-container>
         <nuxt />
         <v-row justify="center" align="center" class="py-12">
-          <v-col cols="11" sm="10" md="8" lg="7" class="white--text">
+          <v-col cols="11" sm="10" md="8" lg="7" class="footer">
             <v-row  justify="center" align="center">
               <v-col cols="12" md="5" class="logo" justify="center" align="center">
                 <v-avatar size="64">
-                  <img src="~/static/icon.png"/>
+                  <img src="~/static/icon.png" alt="icon"/>
                 </v-avatar>
               </v-col>
               <v-divider :vertical="$vuetify.breakpoint.mdAndUp"/>
               <v-col cols="12" md="5" :align="$vuetify.breakpoint.mdAndUp ? 'start' : 'center'">
                 <p>takumma</p>
                 <p>NIT SC</p>
-                <a href="https://github.com/takumma">GitHub</a>
-                <a href="https://twitter.com/tak_kum_ma">Twitter</a>
-                <a href="https://www.takumma.net/">Portfolio</a>
+                <div class="links">
+                  <a href="https://github.com/takumma">GitHub</a>
+                  <a href="https://twitter.com/tak_kum_ma">Twitter</a>
+                  <a href="https://www.takumma.net/">Portfolio</a>
+                </div>
               </v-col>
             </v-row>
           </v-col>
@@ -50,7 +52,7 @@ export default class IndexPage extends Vue {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .v-toolbar__content {
   padding: 0;
 }
@@ -71,12 +73,15 @@ header {
   background-color: #263238;
 }
 
-.v-divider {
-    border-color: white;
-}
-
-a {
+.footer {
   color: white;
-  border-bottom: solid 1px;
+  
+  .v-divider {
+    border-color: white;
+  }
+
+  .links a {
+    color: white;
+  }
 }
 </style>
